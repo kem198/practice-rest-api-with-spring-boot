f024e9c3ee803450607a7fa725b8cec03a751d2e
kem198 <kenkenpa198@gmail.com>
2025-04-25 16:10:38 +0900

    add initial FizzBuzzTests class with placeholder test

---
+++ src/test/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzzTests.java
@@ -0,0 +1,14 @@
+package dev.kem198.practice_spring_boot_rest_api.service;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTests {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
