72d19a8119888e9a29e00e2a9ae7ea38dbf8de56
kem198 <kenkenpa198@gmail.com>
2025-04-25 11:17:54 +0900

    first commit by Spring Initializr

---
+++ src/main/java/dev/kem198/practice_spring_boot_rest_api/PracticeSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.kem198.practice_spring_boot_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kem198/practice_spring_boot_rest_api/PracticeSpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kem198.practice_spring_boot_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
