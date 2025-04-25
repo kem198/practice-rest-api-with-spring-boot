7165a7de795a34d8decd2a2a6bbe14ec7ce93948
kem198 <kenkenpa198@gmail.com>
2025-04-25 16:41:34 +0900

    implement FizzBuzz class and add initial test for conversion

---
+++ src/main/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzz.java
@@ -0,0 +1,9 @@
+package dev.kem198.practice_spring_boot_rest_api.service;
+
+public class FizzBuzz {
+
+    public String convert(int i) {
+        return "1";
+    }
+
+}
--- src/test/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzzTests.java
+++ src/test/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzzTests.java
@@ -1,14 +1,25 @@
 package dev.kem198.practice_spring_boot_rest_api.service;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTests {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    @DisplayName("1 を渡すと 1 が返ること")
+    void shouldReturn1WhenPassed1() {
+        // Arrange (準備)
+        FizzBuzz fizzbuzz = new FizzBuzz(); // ★3 ここから書く！「準備」から書く！
+
+        // Act (実行)
+        // ★2 使う側の視点でメソッド名を考える！
+        String actual = fizzbuzz.convert(1);
+
+        // Assert (検証)
+        // ★1 ここから書く！「期待値」から書く！
+        assertEquals("1", actual);
     }
 
 }
