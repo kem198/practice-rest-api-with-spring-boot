4051feecf9537570b527910b4de3c4c5de918812
kem198 <kenkenpa198@gmail.com>
2025-04-25 13:14:39 +0900

    add initial unit tests for GreetingController

---
+++ src/test/java/dev/kem198/practice_spring_boot_rest_api/controller/GreetingControllerTests.java
@@ -0,0 +1,20 @@
+package dev.kem198.practice_spring_boot_rest_api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GreetingControllerTests {
+    @Test
+    @DisplayName("JUnit の動作確認用 成功")
+    void checkTestSuccess() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    @DisplayName("JUnit の動作確認用 失敗")
+    void checkTestFail() {
+        assertEquals(1, 0);
+    }
+}
