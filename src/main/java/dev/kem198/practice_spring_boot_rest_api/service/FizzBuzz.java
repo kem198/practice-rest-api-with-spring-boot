f74b654c7e6e2ea4efb77bff826210ede8914d4f
kem198 <kenkenpa198@gmail.com>
2025-04-25 17:11:36 +0900

    fix FizzBuzz conversion logic to handle all multiples of 3; update tests for clarity and correctness

--- src/main/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzz.java
+++ src/main/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzz.java
@@ -3,7 +3,7 @@ package dev.kem198.practice_spring_boot_rest_api.service;
 public class FizzBuzz {
 
     public String convert(int num) {
-        if (num == 3) {
+        if (num % 3 == 0) {
             return "Fizz";
         }
         return String.valueOf(num);
--- src/test/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzzTests.java
+++ src/test/java/dev/kem198/practice_spring_boot_rest_api/service/FizzBuzzTests.java
@@ -2,38 +2,34 @@ package dev.kem198.practice_spring_boot_rest_api.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTests {
 
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void BeforeEach() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     @DisplayName("1 を渡すと文字列 1 を返すこと")
     void shouldReturn1WhenPassed1() {
-        // Arrange
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
-        // Act & Assert
-        assertEquals("1", fizzbuzz.convert(1));
+        assertEquals("1", fizzBuzz.convert(1));
     }
 
     @Test
     @DisplayName("2 を渡すと文字列 2 を返すこと")
     void shouldReturn2WhenPassed2() {
-        // Arrange
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
-        // Act & Assert
-        assertEquals("2", fizzbuzz.convert(2));
+        assertEquals("2", fizzBuzz.convert(2));
     }
 
     @Test
     @DisplayName("3 を渡すと文字列 Fizz を返すこと")
     void shouldReturnFizzWhenPassed3() {
-        // Arrange
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
-        // Act & Assert
-        assertEquals("2", fizzbuzz.convert(2));
+        assertEquals("2", fizzBuzz.convert(2));
     }
 }
