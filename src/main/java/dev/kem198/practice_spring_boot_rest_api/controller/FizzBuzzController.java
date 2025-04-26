67e2bdc5b0a74c015ba5ee1e043ae08fab702bc7
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-26 09:08:25 +0900

    add RequestParam annotation to execute method in FizzBuzzController

--- src/main/java/dev/kem198/practice_spring_boot_rest_api/controller/FizzBuzzController.java
+++ src/main/java/dev/kem198/practice_spring_boot_rest_api/controller/FizzBuzzController.java
@@ -1,8 +1,12 @@
 package dev.kem198.practice_spring_boot_rest_api.controller;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FizzBuzzController {
 
-    public String execute(String num) {
+    public String execute(@RequestParam(value = "num") String num) {
         return "{\"result\": \"Fizz\"}";
     }
 
