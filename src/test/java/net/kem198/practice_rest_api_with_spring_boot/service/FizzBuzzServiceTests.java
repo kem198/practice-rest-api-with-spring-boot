d0e6386055f7eb6bef82b1271d5e15c557828e99
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-28 00:17:04 +0900

    refactor: update display name for test methods in FizzBuzzServiceTests for clarity

--- src/test/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzServiceTests.java
+++ src/test/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzServiceTests.java
@@ -1,13 +1,14 @@
 package net.kem198.practice_rest_api_with_spring_boot.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzServiceTests {
     @Test
-    @DisplayName("数値へ変換可能な文字列であれば FizzBuzzUtils.convert() を一回呼び結果を返す")
+    @DisplayName("数値文字列であれば FizzBuzzUtils.convert() を一回呼び結果を返す")
     void returnsResultForNumericString() {
         // Arrange
         FizzBuzzService fizzBuzzService = new FizzBuzzService();
@@ -17,5 +18,21 @@ public class FizzBuzzServiceTests {
 
         // Assert
         assertEquals("Fizz", actual);
+    }
+
+    @Test
+    @DisplayName("数値文字列でなければ例外 IllegalArgumentException をスローすること")
+    void throwIllegalArgumentExceptionNonNumericString() {
+        // Arrange
+        FizzBuzzService fizzBuzzService = new FizzBuzzService();
+
+        // Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            fizzBuzzService.processFizzBuzz("abc");
+        });
+
+        // Assert
+        // assertThrows() は継承元クラスが一致していればパスしてしまうので例外クラスを明示的にチェックする
+        assertEquals(IllegalArgumentException.class, exception.getClass());
     }
 }
