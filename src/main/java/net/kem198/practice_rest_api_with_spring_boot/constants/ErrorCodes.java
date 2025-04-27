aecbb85e7db948517c56addc958bcda468654bf0
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-28 05:42:51 +0900

    refactor: simplify ErrorCodes and ErrorResponseDto by removing redundant methods and adding Lombok annotations

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/constants/ErrorCodes.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/constants/ErrorCodes.java
@@ -1,5 +1,8 @@
 package net.kem198.practice_rest_api_with_spring_boot.constants;
 
+import lombok.Getter;
+
+@Getter
 public enum ErrorCodes {
     INVALID_NUMBER_FORMAT("1000"),
     MISSING_PARAMETER("1001"),
@@ -9,9 +12,5 @@ public enum ErrorCodes {
 
     ErrorCodes(String code) {
         this.code = code;
-    }
-
-    public String getCode() {
-        return code;
     }
 }
