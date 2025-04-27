56dc29b4db5d71b9c744425971147b72c58335bf
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-27 23:38:49 +0900

    feat: implement FizzBuzz processing in FizzBuzzService using FizzBuzzUtils

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzService.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzService.java
@@ -1,7 +1,9 @@
 package net.kem198.practice_rest_api_with_spring_boot.service;
 
+import net.kem198.practice_rest_api_with_spring_boot.utils.FizzBuzzUtils;
+
 public class FizzBuzzService {
     public String processFizzBuzz(String numberString) {
-        return "Fizz";
+        return FizzBuzzUtils.convert(Integer.parseInt(numberString));
     }
 }
