2f978b0120444da1f2ff2648b90944e33bd4d002
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-27 23:45:58 +0900

    refactor: improve readability of processFizzBuzz method in FizzBuzzService

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzService.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzService.java
@@ -4,6 +4,7 @@ import net.kem198.practice_rest_api_with_spring_boot.utils.FizzBuzzUtils;
 
 public class FizzBuzzService {
     public String processFizzBuzz(String numberString) {
-        return FizzBuzzUtils.convert(Integer.parseInt(numberString));
+        int number = Integer.parseInt(numberString);
+        return FizzBuzzUtils.convert(number);
     }
 }
