f954c4b819a50c46f4002a2abf33d82a6fa649af
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-28 02:45:09 +0900

    refactor: update GreetingController to streamline endpoint mapping

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/controller/GreetingController.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/controller/GreetingController.java
@@ -3,18 +3,20 @@ package net.kem198.practice_rest_api_with_spring_boot.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.kem198.practice_rest_api_with_spring_boot.model.Greeting;
 
 @RestController
+@RequestMapping("/api/v1/greeting")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/api/v1/greeting")
+    @GetMapping
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         System.out.println("aaa");
         System.out.println("bbb");
