709d29c1c46416c790797eca67ad21c3df3e0d6f
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-28 05:58:29 +0900

    refactor: organize test classes by adding nested structure to GreetingControllerTests

--- src/test/java/net/kem198/practice_rest_api_with_spring_boot/controller/GreetingControllerTests.java
+++ src/test/java/net/kem198/practice_rest_api_with_spring_boot/controller/GreetingControllerTests.java
@@ -3,6 +3,7 @@ package net.kem198.practice_rest_api_with_spring_boot.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,7 @@ public class GreetingControllerTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Nested
     class GetRequestTests {
         @Test
         @DisplayName("デフォルトの名前で挨拶を返す")
