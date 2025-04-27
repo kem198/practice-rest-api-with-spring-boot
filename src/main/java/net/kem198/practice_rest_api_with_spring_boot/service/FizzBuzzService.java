b2291c3e4a4fc5f4fc96655024aa35caaa4a52c1
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-28 00:19:27 +0900

    refactor: add error handling for invalid number strings in processFizzBuzz method

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzService.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/service/FizzBuzzService.java
@@ -4,7 +4,11 @@ import net.kem198.practice_rest_api_with_spring_boot.utils.FizzBuzzUtils;
 
 public class FizzBuzzService {
     public String processFizzBuzz(String numberString) {
-        int number = Integer.parseInt(numberString);
-        return FizzBuzzUtils.convert(number);
+        try {
+            int number = Integer.parseInt(numberString);
+            return FizzBuzzUtils.convert(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
