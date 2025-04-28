7f0a317b3cf0f228e240939614315e36229f566e
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-28 12:22:57 +0900

    refactor: simplify GreetingController by removing unused variables and leveraging GreetingService

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/controller/GreetingController.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/controller/GreetingController.java
@@ -1,26 +1,23 @@
 package net.kem198.practice_rest_api_with_spring_boot.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.kem198.practice_rest_api_with_spring_boot.dto.GreetingDto;
+import net.kem198.practice_rest_api_with_spring_boot.service.GreetingService;
 
 @RestController
 @RequestMapping("/api/v1/greeting")
 public class GreetingController {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    @Autowired
+    private GreetingService GreetingService;
 
     @GetMapping
     public GreetingDto getContent(@RequestParam(value = "name", defaultValue = "World") String name) {
-        System.out.println("aaa");
-        System.out.println("bbb");
-        GreetingDto greetingDto = new GreetingDto(counter.incrementAndGet(), String.format(template, name));
+        GreetingDto greetingDto = GreetingService.processGreeting(name);
 
         return greetingDto;
     }
