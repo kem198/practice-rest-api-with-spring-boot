7e59253542645a5906aa116f8875a2900be1cb70
kem198 <kenkenpa198@gmail.com>
2025-05-06 15:54:45 +0900

    feat: add exception handling for invalid number format in FizzBuzzRestController

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/fizzbuzz/FizzBuzzRestController.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/fizzbuzz/FizzBuzzRestController.java
@@ -3,12 +3,17 @@ package net.kem198.practice_rest_api_with_spring_boot.api.fizzbuzz;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import net.kem198.practice_rest_api_with_spring_boot.constants.ErrorTitles;
 import net.kem198.practice_rest_api_with_spring_boot.domain.service.fizzbuzz.FizzBuzzService;
 
 @RestController
@@ -21,5 +26,13 @@ public class FizzBuzzRestController {
     public ResponseEntity<Map<String, String>> convert(@RequestParam(value = "num") int number) {
         String result = fizzBuzzService.processFizzBuzz(number);
         return ResponseEntity.ok(Map.of("result", result));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle(ErrorTitles.INVALID_NUMBER_FORMAT.getTitle());
+        problemDetail.setDetail(String.format("The '%s' query parameter must be a valid integer.", ex.getName()));
+        return problemDetail;
     }
 }
--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/constants/ErrorTitles.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/constants/ErrorTitles.java
@@ -2,7 +2,6 @@ package net.kem198.practice_rest_api_with_spring_boot.constants;
 
 public enum ErrorTitles {
     INVALID_NUMBER_FORMAT("Invalid Number Format"),
-    MISSING_PARAMETER("Missing Parameter"),
     GENERIC_ERROR("Generic Error");
 
     private final String title;
