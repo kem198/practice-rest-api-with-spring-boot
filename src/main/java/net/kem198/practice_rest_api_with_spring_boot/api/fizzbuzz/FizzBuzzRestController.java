b7838a2d1ddfaf01c68809eb7f7aff88af759041
kem198 <kenkenpa198@gmail.com>
2025-05-06 15:15:11 +0900

    refactor: remove unused imports in FizzBuzzRestController for cleaner code

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/fizzbuzz/FizzBuzzRestController.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/fizzbuzz/FizzBuzzRestController.java
@@ -1,21 +1,14 @@
 package net.kem198.practice_rest_api_with_spring_boot.api.fizzbuzz;
 
-import java.net.URI;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import net.kem198.practice_rest_api_with_spring_boot.constants.ErrorTitles;
 import net.kem198.practice_rest_api_with_spring_boot.domain.service.fizzbuzz.FizzBuzzService;
 
 @RestController
