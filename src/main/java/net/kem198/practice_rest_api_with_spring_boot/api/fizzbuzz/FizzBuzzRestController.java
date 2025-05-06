c9bfe99ab3d53e8ed23a5f3dd1668c95122b2dba
kem198 <kenkenpa198@gmail.com>
2025-05-06 17:48:36 +0900

    refactor: remove unused import for Map in FizzBuzzRestController

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/fizzbuzz/FizzBuzzRestController.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/fizzbuzz/FizzBuzzRestController.java
@@ -1,7 +1,5 @@
 package net.kem198.practice_rest_api_with_spring_boot.api.fizzbuzz;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
