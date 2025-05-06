3b8589656713365bbd7bc53b9012ae1a8faf9f38
kem198 <kenkenpa198@gmail.com>
2025-05-06 17:37:25 +0900

    fix: update handleBusinessException method to return ResponseEntity<ProblemDetail> for improved type safety

--- src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/common/error/RestGlobalExceptionHandler.java
+++ src/main/java/net/kem198/practice_rest_api_with_spring_boot/api/common/error/RestGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package net.kem198.practice_rest_api_with_spring_boot.api.common.error;
 
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,7 +12,7 @@ import net.kem198.practice_rest_api_with_spring_boot.domain.exception.common.Bus
 public class RestGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<Object> handleBusinessException(BusinessException ex) {
+    public ResponseEntity<ProblemDetail> handleBusinessException(BusinessException ex) {
         return ResponseEntity
                 .status(ex.getProblemDetail().getStatus())
                 .body(ex.getProblemDetail());
