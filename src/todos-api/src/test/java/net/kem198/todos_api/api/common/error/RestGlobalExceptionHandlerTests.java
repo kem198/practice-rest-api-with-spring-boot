b621956ef542ab5474492b839c5b68b9edfde324
kem198 <kenkenpa198@gmail.com>
2025-05-17 16:11:51 +0900

    test: add validation error detail assertions in RestGlobalExceptionHandlerTests

--- src/todos-api/src/test/java/net/kem198/todos_api/api/common/error/RestGlobalExceptionHandlerTests.java
+++ src/todos-api/src/test/java/net/kem198/todos_api/api/common/error/RestGlobalExceptionHandlerTests.java
@@ -2,6 +2,9 @@ package net.kem198.todos_api.api.common.error;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -114,6 +117,44 @@ public class RestGlobalExceptionHandlerTests {
             JsonNode responseBody = objectMapper.readTree(response.getBody());
             JsonNode errors = responseBody.get("errors");
             assertEquals(2, errors.size());
+        }
+
+        @Test
+        @DisplayName("引数のバリデーションエラー要素に所定のエラー詳細内容を含む")
+        void returnsErrorsWithContainedErrorDetails()
+                throws JsonMappingException, JsonProcessingException {
+            // Arrange
+            String requestBody = """
+                        {
+                            "todoDescription": "123456789012345678901234567890123456789012345678901"
+                        }
+                    """;
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+            // Act
+            ResponseEntity<String> response = restTemplate.postForEntity("/v1/todos", requestEntity, String.class);
+
+            // Assert
+            JsonNode responseBody = objectMapper.readTree(response.getBody());
+            JsonNode errors = responseBody.get("errors");
+            assertTrue(
+                    StreamSupport.stream(errors.spliterator(), false)
+                            .anyMatch(e -> "todoTitle".equals(e.get("field").asText())
+                                    && e.get("rejectedValue").isNull()
+                                    && "NotNull".equals(e.get("code").asText())
+                                    && "todoResource".equals(e.get("objectName").asText())
+                                    && "must not be null".equals(e.get("message").asText())));
+            assertTrue(
+                    StreamSupport.stream(errors.spliterator(), false)
+                            .anyMatch(e -> "todoDescription".equals(e.get("field").asText())
+                                    && "123456789012345678901234567890123456789012345678901"
+                                            .equals(e.get("rejectedValue").asText())
+                                    && "Size".equals(e.get("code").asText())
+                                    && "todoResource".equals(e.get("objectName").asText())
+                                    && "size must be between 1 and 50".equals(e.get("message").asText())));
         }
     }
 }
