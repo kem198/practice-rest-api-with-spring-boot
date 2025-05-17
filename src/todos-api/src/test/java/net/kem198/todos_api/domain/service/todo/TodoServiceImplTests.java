690963e49b8eccbe4955bf15ea4e6f8b7674519c
kem198 <kenkenpa198@gmail.com>
2025-05-17 17:57:55 +0900

    test: enhance TodoServiceImplTests with nested test structure and assertions for findOne method

--- src/todos-api/src/test/java/net/kem198/todos_api/domain/service/todo/TodoServiceImplTests.java
+++ src/todos-api/src/test/java/net/kem198/todos_api/domain/service/todo/TodoServiceImplTests.java
@@ -1,12 +1,47 @@
 package net.kem198.todos_api.domain.service.todo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import net.kem198.todos_api.domain.exception.common.ResourceNotFoundException;
+import net.kem198.todos_api.domain.model.Todo;
+
+@SpringBootTest
 public class TodoServiceImplTests {
-    @Test
-    public void assertCheck() {
-        assertEquals(1, 1);
+    @Autowired
+    TodoService todoService;
+
+    @Nested
+    class FindOneTests {
+        @Test
+        @DisplayName("引数の todoId と一致する Todo を返す")
+        public void returnsTodoWithMatchingTodoId() {
+            // Arrange
+            Todo expectedTodo = new Todo();
+            todoService.create(expectedTodo);
+
+            // Act
+            Todo todo = todoService.findOne(expectedTodo.getTodoId());
+
+            // Assert
+            assertEquals(expectedTodo, todo);
+        }
+
+        @Test
+        @DisplayName("引数の todoId と一致する Todo が存在しなければ ResourceNotFoundException をスローする")
+        public void throwsResourceNotFoundExceptionWithNonMatchingTodoId() {
+            // Act & Assert
+            assertThrows(ResourceNotFoundException.class, () -> {
+                todoService.findOne("1");
+            });
+        }
+
     }
+
 }
