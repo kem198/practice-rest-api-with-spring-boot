f5ba9d68b4284652ec78a5775af0de380d398a02
kem198 <kenkenpa198@gmail.com>
2025-05-17 16:38:29 +0900

    test: add initial test classes for TodoRepositoryImpl and TodoServiceImplTests

---
+++ src/todos-api/src/test/java/net/kem198/todos_api/domain/service/todo/TodoServiceImplTests.java
@@ -0,0 +1,12 @@
+package net.kem198.todos_api.domain.service.todo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TodoServiceImplTests {
+    @Test
+    public void assertCheck() {
+        assertEquals(1, 1);
+    }
+}
