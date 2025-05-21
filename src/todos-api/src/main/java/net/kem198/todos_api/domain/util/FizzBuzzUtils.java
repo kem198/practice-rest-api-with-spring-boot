798490f1b1cd10e7beebdedd194f10377746abbb
kem198 <kenkenpa198@gmail.com>
2025-05-22 07:22:55 +0900

    Commit with intentional mistake

--- src/todos-api/src/main/java/net/kem198/todos_api/domain/util/FizzBuzzUtils.java
+++ src/todos-api/src/main/java/net/kem198/todos_api/domain/util/FizzBuzzUtils.java
@@ -5,11 +5,11 @@ public class FizzBuzzUtils {
         if (num % 3 == 0) {
             return "Fizz";
         }
-        if (num % 5 == 0) {
-            return "Buzz";
-        }
         if (num % 3 == 0 && num % 5 == 0) {
             return "FizzBuzz";
+        }
+        if (num % 5 == 0) {
+            return "Buzz";
         }
         return String.valueOf(num);
     }
